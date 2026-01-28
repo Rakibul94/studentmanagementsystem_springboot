86ffcbb1e9b1ba075e0d74e8ce4b46ab7dca1576
Rakibul94 <rakibulred123@gmail.com>
2026-01-28 17:30:16 +0600

    Initial Commit

--- src/main/java/com/studentproject/studentmanagementsystem/Model/Student.java
+++ src/main/java/com/studentproject/studentmanagementsystem/Model/Student.java
@@ -1,81 +1,26 @@
 package com.studentproject.studentmanagementsystem.Model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "students")
+@Data                   // Generates getters, setters, toString, equals, hashCode
+@NoArgsConstructor      // Required by JPA
+@AllArgsConstructor     // Generates all-args constructor
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @Column(name = "student_id", unique = true, nullable = false)
     private int studentId;
+
     private String name;
     private double cgpa;
     private String email;
     private String program;
-
-    public Student() {
-    } // Required by JPA
-
-    public Student(Integer id, int studentId, String name, double cgpa, String email, String program) {
-        this.id = id;
-        this.studentId = studentId;
-        this.name = name;
-        this.cgpa = cgpa;
-        this.email = email;
-        this.program = program;
-    }
-
-    // Getters
-    public Integer getId() {
-        return id;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getCgpa() {
-        return cgpa;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getProgram() {
-        return program;
-    }
-
-    //
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCgpa(double cgpa) {
-        this.cgpa = cgpa;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setProgram(String program) {
-        this.program = program;
-    }
-
 }
